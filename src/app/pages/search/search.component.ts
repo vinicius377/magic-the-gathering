954f86d44b62fe278efb6f28fb321895893ba42a
Vinicius <viniciusep181@gmail.com>
2024-05-14 23:17:18 -0300

    feat:search page

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+
+export class SearchComponent {
+  constructor(private router: Router) { }
+
+  searchParams!: FormGroup
+
+  blockOptions = ['Amonkhet', 'Ixalan', 'Zendikar', 'Ravnica', 'Onslaught'];
+
+  onSubmit() {
+    if (this.searchParams.invalid) return
+    this.router.navigate(['sets'], { queryParams: this.searchParams.value });
+  }
+
+  ngOnInit() {
+      this.searchParams = new FormGroup({
+        name: new FormControl(''),
+        block: new FormControl('', [Validators.required])
+      })
+  }
+}
