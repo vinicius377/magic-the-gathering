51e8f4353f9ee8907d64208710d872faf2ad5a24
Vinicius <viniciusep181@gmail.com>
2024-05-14 23:16:48 -0300

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'linked';
-}
+
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { SetListComponent } from './pages/set-list/set-list.component';
+import { CardsComponent } from './pages/cards/cards.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent,
+    title: 'Buscar conjuntos de cards'
+  },
+  {
+    path: 'sets',
+    component: SetListComponent,
+    title: 'Conjunto de cards'
+  },
+  {
+    path: 'cards/:setId',
+    component: CardsComponent,
+    title: (activedRouter) => {
+      const setId = activedRouter.paramMap.get('setId')
+      return `Cards para ${setId}`
+    }
+  }
+];
